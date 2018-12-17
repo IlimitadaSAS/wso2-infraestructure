13a628d6d26aa486b42c8599296730fe3dead42f
Miguel Rios <mrios1980@gmail.com>
2018-12-17 09:32:32 -0500

    Init repo develop

---
+++ common/kubernetes-membership-scheme/src/main/java/org/wso2/carbon/membership/scheme/kubernetes/KubernetesMembershipScheme.java
@@ -0,0 +1,169 @@
+/*
+* Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.wso2.carbon.membership.scheme.kubernetes;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.config.TcpIpConfig;
+import com.hazelcast.core.*;
+import org.apache.axis2.clustering.ClusteringFault;
+import org.apache.axis2.clustering.ClusteringMessage;
+import org.apache.axis2.description.Parameter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.core.clustering.hazelcast.HazelcastCarbonClusterImpl;
+import org.wso2.carbon.core.clustering.hazelcast.HazelcastMembershipScheme;
+import org.wso2.carbon.core.clustering.hazelcast.HazelcastUtil;
+import org.wso2.carbon.membership.scheme.kubernetes.exceptions.KubernetesMembershipSchemeException;
+import org.wso2.carbon.membership.scheme.kubernetes.resolver.AddressResolver;
+import org.wso2.carbon.membership.scheme.kubernetes.resolver.ApiBasedPodIpResolver;
+import org.wso2.carbon.membership.scheme.kubernetes.resolver.DNSBasedPodIpResolver;
+import org.wso2.carbon.utils.xml.StringUtils;
+
+import java.util.*;
+
+/**
+ * Kubernetes membership scheme provides carbon cluster discovery on kubernetes.
+ */
+public class KubernetesMembershipScheme implements HazelcastMembershipScheme {
+
+    private static final Log log = LogFactory.getLog(KubernetesMembershipScheme.class);
+
+    private final Map<String, Parameter> parameters;
+    private final NetworkConfig nwConfig;
+    private final List<ClusteringMessage> messageBuffer;
+    private HazelcastInstance primaryHazelcastInstance;
+    private HazelcastCarbonClusterImpl carbonCluster;
+
+    public KubernetesMembershipScheme(Map<String, Parameter> parameters, String primaryDomain, Config config,
+            HazelcastInstance primaryHazelcastInstance, List<ClusteringMessage> messageBuffer) {
+        this.parameters = parameters;
+        this.primaryHazelcastInstance = primaryHazelcastInstance;
+        this.messageBuffer = messageBuffer;
+        this.nwConfig = config.getNetworkConfig();
+    }
+
+    @Override public void setPrimaryHazelcastInstance(HazelcastInstance primaryHazelcastInstance) {
+        this.primaryHazelcastInstance = primaryHazelcastInstance;
+    }
+
+    @Override public void setLocalMember(Member localMember) {
+    }
+
+    @Override public void setCarbonCluster(HazelcastCarbonClusterImpl hazelcastCarbonCluster) {
+        this.carbonCluster = hazelcastCarbonCluster;
+    }
+
+    @Override public void init() throws ClusteringFault {
+        try {
+            log.info("Initializing kubernetes membership scheme...");
+
+            nwConfig.getJoin().getMulticastConfig().setEnabled(false);
+            nwConfig.getJoin().getAwsConfig().setEnabled(false);
+            TcpIpConfig tcpIpConfig = nwConfig.getJoin().getTcpIpConfig();
+            tcpIpConfig.setEnabled(true);
+
+            String useDns = System.getenv(Constants.USE_DNS);
+            if (StringUtils.isEmpty(useDns)) {
+                useDns = getParameterValue(Constants.USE_DNS, "true");
+            }
+
+            AddressResolver podIpResolver;
+            Set<String> containerIPs;
+            if (Boolean.parseBoolean(useDns)) {
+                log.debug("Using DNS based pod ip resolving method");
+                podIpResolver = new DNSBasedPodIpResolver(parameters);
+            } else {
+                log.debug("Using API based pod ip resolving method");
+                podIpResolver = new ApiBasedPodIpResolver(parameters);
+            }
+            containerIPs = podIpResolver.resolveAddresses();
+            // if no IPs are found, can't initialize clustering
+            if (containerIPs.isEmpty()) {
+                throw new KubernetesMembershipSchemeException("No member ips found, unable to initialize the "
+                        + "Kubernetes membership scheme");
+            }
+
+            for (String containerIP : containerIPs) {
+                tcpIpConfig.addMember(containerIP);
+                log.info("Member added to cluster configuration: [container-ip] " + containerIP);
+            }
+            log.info("Kubernetes membership scheme initialized successfully");
+
+        } catch (Exception e) {
+            String errorMsg = "Kubernetes membership initialization failed";
+            log.error(errorMsg, e);
+            throw new ClusteringFault(errorMsg, e);
+        }
+    }
+
+    private String getParameterValue(String parameterName, String defaultValue) throws
+            KubernetesMembershipSchemeException {
+        Parameter kubernetesServicesParam = getParameter(parameterName);
+        if (kubernetesServicesParam == null) {
+            if (defaultValue == null) {
+                throw new KubernetesMembershipSchemeException(parameterName + " parameter not found");
+            } else {
+                return defaultValue;
+            }
+        }
+        return (String) kubernetesServicesParam.getValue();
+    }
+
+    @Override public void joinGroup() throws ClusteringFault {
+        primaryHazelcastInstance.getCluster().addMembershipListener(new KubernetesMembershipSchemeListener());
+    }
+
+    private Parameter getParameter(String name) {
+        return parameters.get(name);
+    }
+
+    /**
+     * Kubernetes membership scheme listener
+     */
+    private class KubernetesMembershipSchemeListener implements MembershipListener {
+
+        @Override public void memberAdded(MembershipEvent membershipEvent) {
+            Member member = membershipEvent.getMember();
+
+            // Send all cluster messages
+            carbonCluster.memberAdded(member);
+            log.info(String.format("Member joined: [UUID] %s, [Address] %s", member.getUuid(),
+                    member.getSocketAddress().toString()));
+            // Wait for sometime for the member to completely join before replaying messages
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ignored) {
+            }
+            HazelcastUtil.sendMessagesToMember(messageBuffer, member, carbonCluster);
+        }
+
+        @Override public void memberRemoved(MembershipEvent membershipEvent) {
+            Member member = membershipEvent.getMember();
+            carbonCluster.memberRemoved(member);
+            log.info(String.format("Member left: [UUID] %s, [Address] %s", member.getUuid(),
+                    member.getSocketAddress().toString()));
+        }
+
+        @Override public void memberAttributeChanged(MemberAttributeEvent memberAttributeEvent) {
+            if (log.isDebugEnabled()) {
+                log.debug(String.format("Member attribute changed: [Key] %s, [Value] %s", memberAttributeEvent.getKey(),
+                        memberAttributeEvent.getValue()));
+            }
+        }
+    }
+}
---
+++ common/kubernetes-membership-scheme/src/test/java/org/wso2/carbon/membership/scheme/kubernetes/test/ReadinessProbeEndpointTestCases.java
@@ -0,0 +1,216 @@
+/*
+* Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.wso2.carbon.membership.scheme.kubernetes.test;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.apache.axis2.clustering.ClusteringMessage;
+import org.apache.axis2.description.Parameter;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.Header;
+import org.mockserver.model.HttpRequest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.wso2.carbon.membership.scheme.kubernetes.Constants;
+import org.wso2.carbon.membership.scheme.kubernetes.KubernetesMembershipScheme;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.testng.Assert.assertEquals;
+import static org.wso2.carbon.membership.scheme.kubernetes.test.KubernetesAPIMockServer.MOCK_SERVER_PORT;
+
+/**
+ * Integration tests for kubernetes membership scheme
+ */
+public class ReadinessProbeEndpointTestCases {
+    private static final Log log = LogFactory.getLog(ReadinessProbeEndpointTestCases.class);
+    private ClientAndServer mockServer;
+    private String namespace = "default";
+    private String services = "wso2is-default";
+
+    @BeforeTest
+    public void init() throws Exception {
+        mockServer = KubernetesAPIMockServer.getMockServer();
+
+    }
+
+    @Test
+    public void testEndpointAddressesOnly() throws Exception {
+        // test Endpoint with Addresses Only
+        log.info("Executing test case with Addresses Only ");
+        registerAPI("/ReadinessProbeEndpointTestCases/addressOnly.json");
+        assertEquals(initializeMembershipScheme().size(), 2, "Endpoint with Addresses Only");
+    }
+
+    @Test
+    public void testEndpointAddressesOnlyWithAuthBasic() throws Exception {
+        // test Endpoint with Addresses Only
+        log.info("Executing test case with Addresses Only With auth basic");
+        registerAPI("/ReadinessProbeEndpointTestCases/addressOnly.json");
+        assertEquals(initializeMembershipScheme(AuthType.BASIC).size(), 2, "Endpoint with Addresses Only");
+    }
+
+    @Test
+    public void testEndpointAddressesOnlyWithAuthBearer() throws Exception {
+        // test Endpoint with Addresses Only
+        log.info("Executing test case with Addresses Only With auth bearer");
+        registerAPI("/ReadinessProbeEndpointTestCases/addressOnly.json");
+        assertEquals(initializeMembershipScheme(AuthType.BEARER).size(), 2, "Endpoint with Addresses Only");
+    }
+
+    @Test
+    public void testEndpointNotReadyAddressesOnly() throws Exception {
+        // test Endpoint with notReadyAddresses Only
+        log.info("Executing test case with notReadyAddresses Only ");
+        registerAPI("/ReadinessProbeEndpointTestCases/notReadyAddressOnly.json");
+        assertEquals(initializeMembershipScheme().size(), 2, "Endpoint with notReadyAddresses Only");
+    }
+
+    @Test
+    public void testEndpointNotReadyAddressesOnlyWithAuthBasic() throws Exception {
+        // test Endpoint with notReadyAddresses Only
+        log.info("Executing test case with notReadyAddresses Only with auth basic");
+        registerAPI("/ReadinessProbeEndpointTestCases/notReadyAddressOnly.json");
+        assertEquals(initializeMembershipScheme(AuthType.BASIC).size(), 2, "Endpoint with notReadyAddresses Only");
+    }
+
+    @Test
+    public void testEndpointNotReadyAddressesOnlyWithAuthBearer() throws Exception {
+        // test Endpoint with notReadyAddresses Only
+        log.info("Executing test case with notReadyAddresses Only with auth bearer");
+        registerAPI("/ReadinessProbeEndpointTestCases/notReadyAddressOnly.json");
+        assertEquals(initializeMembershipScheme(AuthType.BEARER).size(), 2, "Endpoint with notReadyAddresses Only");
+    }
+
+    @Test
+    public void testEndpointNotReadyAddressesAndAddresses() throws Exception {
+        // test Endpoint with NotReadyAddresses And Addresses
+        log.info("Executing test case NotReadyAddresses And Addresses");
+        registerAPI("/ReadinessProbeEndpointTestCases/notReadyAddressAndAddress.json");
+        assertEquals(initializeMembershipScheme().size(), 2, "Endpoint with notReadyAddress And Address");
+
+    }
+
+    @Test
+    public void testEndpointNotReadyAddressesAndAddressesWithAuthBasic() throws Exception {
+        // test Endpoint with NotReadyAddresses And Addresses
+        log.info("Executing test case NotReadyAddresses And Addresses with auth basic");
+        registerAPI("/ReadinessProbeEndpointTestCases/notReadyAddressAndAddress.json");
+        assertEquals(initializeMembershipScheme(AuthType.BASIC).size(), 2, "Endpoint with notReadyAddress And Address");
+
+    }
+
+    @Test
+    public void testEndpointNotReadyAddressesAndAddressesWithAuthBearer() throws Exception {
+        // test Endpoint with NotReadyAddresses And Addresses
+        log.info("Executing test case NotReadyAddresses And Addresses with auth bearer");
+        registerAPI("/ReadinessProbeEndpointTestCases/notReadyAddressAndAddress.json");
+        assertEquals(initializeMembershipScheme(AuthType.BEARER).size(), 2, "Endpoint with notReadyAddress And Address");
+
+    }
+
+    private void registerAPI(String endpointJSONPath) throws IOException {
+        registerAPI(endpointJSONPath, AuthType.NONE);
+    }
+
+    private void registerAPI(String endpointJSONPath, AuthType authType) throws IOException {
+        // Registering request and response with mock server
+        String endpointJSON = IOUtils.toString(
+                this.getClass().getResourceAsStream(endpointJSONPath), "UTF-8");
+        HttpRequest mockRequest = request()
+                .withPath(String.format(Constants.ENDPOINTS_API_CONTEXT, namespace)
+                        + services)
+                .withMethod("GET");
+        if( authType == AuthType.BASIC ) {
+            mockRequest.withHeader("Authorization","Basic dXNlcm5hbWU6cGFzc3dvcmQ=");
+        } else if(authType == AuthType.BEARER) {
+            mockRequest.withHeader("Authorization","Bearer token");
+        }
+
+        mockServer.when(
+                mockRequest
+
+        ).respond(
+                response()
+                        .withStatusCode(202)
+                        .withHeaders(
+                                new Header("Content-Type", "application/json; charset=utf-8")
+                        )
+                        .withBody(endpointJSON)
+        );
+    }
+
+    private List<String> initializeMembershipScheme() throws Exception {
+        return initializeMembershipScheme(AuthType.NONE);
+    }
+
+    private List<String> initializeMembershipScheme(AuthType bearerAuth) throws Exception {
+        //Initializing membership scheme
+        Config primaryHazelcastConfig;
+        HazelcastInstance primaryHazelcastInstance;
+        Map<String, Parameter> parameters = new HashMap<>();
+        List<ClusteringMessage> messageBuffer;
+        String primaryDomain = "TestDomain";
+        parameters.put(Constants.PARAMETER_NAME_KUBERNETES_NAMESPACE,
+                new Parameter(Constants.PARAMETER_NAME_KUBERNETES_NAMESPACE, namespace));
+        parameters.put(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER,
+                new Parameter(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER,
+                        "http://localhost:" + MOCK_SERVER_PORT));
+        parameters.put(Constants.PARAMETER_NAME_KUBERNETES_SERVICES,
+                new Parameter(Constants.PARAMETER_NAME_KUBERNETES_SERVICES, services));
+        parameters.put(Constants.USE_DNS, new Parameter(Constants.USE_DNS, "false"));
+
+        if(bearerAuth == AuthType.BASIC) {
+            parameters.put(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER_USERNAME,
+                    new Parameter(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER_USERNAME, "username"));
+            parameters.put(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER_PASSWORD,
+                    new Parameter(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER_USERNAME, "password"));
+        }
+
+        if(bearerAuth == AuthType.BEARER) {
+            parameters.put(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER_TOKEN,
+                    new Parameter(Constants.PARAMETER_NAME_KUBERNETES_API_SERVER_TOKEN, "token"));
+        }
+
+        messageBuffer = new ArrayList<>();
+        primaryHazelcastConfig = new Config();
+        primaryHazelcastInstance = Hazelcast.newHazelcastInstance(primaryHazelcastConfig);
+        KubernetesMembershipScheme kubernetesMembershipScheme = new KubernetesMembershipScheme
+                (parameters, primaryDomain, primaryHazelcastConfig, primaryHazelcastInstance,
+                        messageBuffer);
+        kubernetesMembershipScheme.init();
+        log.info("Membership scheme initialized");
+        return primaryHazelcastConfig.getNetworkConfig().getJoin().
+                getTcpIpConfig().getMembers();
+
+    }
+
+    private  enum AuthType {
+        BEARER,
+        BASIC,
+        NONE;
+    }
+}
